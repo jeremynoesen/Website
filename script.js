8d03541c62cc6ec2e293881207f338d9d02eb43f
Jeremy Noesen <jeremynoesen@gmail.com>
2021-05-22 15:52:19 -0500

    more styles

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const sound = "https://f000.backblazeb2.com/b2api/v1/b2_download_file_by_id?fileId=4_z9b2a7e929faa13a473850510_f10306c64124d325b_d20210310_m062616_c000_v0001079_t0021";
 
 let player;
-let changed = false;
+let status = 0;
 let buffer = ["", "", ""];
 
-let styles = `
-              * {
+let style1 = `* {
                 cursor: url(https://ani.cursors-4u.net/games/gam-16/gam1570.cur), url(https://ani.cursors-4u.net/games/gam-16/gam1570.cur), auto !important;
               }
 
@@ -23,28 +22,60 @@ let styles = `
                 filter: brightness(1.1);
               }`;
 
+let style2 = `:root {
+                --background-color: #4DFF00;
+                --box-color: #FFF200;
+                --box-hover-color: #7E05E8;
+                --button-hover-color: #0050ea;
+                --shadow-color: #ff00f3;
+                --text-color: #B54618;
+              }
+              
+              body {
+                font-family: Comic Sans MS;
+              }
+              
+              .notfound {
+                background-image: url(https://i.kym-cdn.com/photos/images/newsfeed/001/018/903/29e.jpg);
+                background-size: 150px;
+                background-repeat: no-repeat;
+                background-position-x: 5%;
+                background-position-y: 50%;
+              }`;
+
 let styleSheet = document.createElement("style");
-styleSheet.type = "text/css";
-styleSheet.innerText = styles;
 
 function keyEvent(event) {
     buffer.shift();
     buffer.push(event.key);
 
     if (buffer.join("") === "sus") {
-        if (changed === false) {
+        if (status !== 1) {
             player.src = sound;
             player.loop = true;
             player.load();
             player.play();
+            styleSheet.innerText = style1;
             document.head.appendChild(styleSheet);
             buffer = ["", "", ""];
-            changed = true;
+            status = 1;
         } else {
             player.pause();
             document.head.removeChild(styleSheet);
             buffer = ["", "", ""];
-            changed = false;
+            status = 0;
+        }
+    } else if (buffer.join("") === "gdp") {
+        if (status !== 2) {
+            player.pause();
+            styleSheet.innerText = style2;
+            document.head.appendChild(styleSheet);
+            buffer = ["", "", ""];
+            status = 2;
+        } else {
+            document.head.removeChild(styleSheet);
+            buffer = ["", "", ""];
+            status = 0;
         }
     }
 }
