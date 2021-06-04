9941a9a5278bf085bb2ae848a83bfe40735ae27c
Jeremy Noesen <jeremynoesen@gmail.com>
2021-06-03 20:04:31 -0500

    scale polishing

--- theme/theme.js
+++ theme/theme.js
@@ -42,13 +42,13 @@ function resizeInit() {
  */
 function resizeEvent() {
     let width = window.innerWidth;
-    if (width <= 1080 && width >= 540) {
+    if (width <= 1080) {
         let size = width - 20;
         let dynPadding = size * (32 / 736); //padding, grid width
         let dynGaps = size * (16 / 736); //gap size, grid width
         let dynShadows = size * (16 / 736); //shadow size, grid width
         let dynRadius = size * (24 / 736); //border radius, grid width
-        let dynFontSize = size * (20 / 736); //font size + 4, grid width
+        let dynFontSize = size * (18 / 736); //font size + 2, grid width
         let dynLineHeight = size * (24 / 736); //line height, grid width
         let dynWideSize = size - (2 * dynPadding);
         let dynLargeSize = (size / 2) - (2 * dynPadding) - (dynGaps / 2);
